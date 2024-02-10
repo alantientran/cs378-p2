8c7a022c12d4f40b35fb48e71d06448d90042b30
att0316698@gmail.com <att0316698@gmail.com>
2024-02-09 21:46:58 -0600

    transferred files

--- src/App.js
+++ src/App.js
@@ -1,92 +1,114 @@
-import './App.css';
-import MenuItem from './components/MenuItem';
+import "./App.css";
+import MenuItem from "./components/MenuItem";
 
-// import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+import "bootstrap/dist/css/bootstrap.min.css"; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+
+// ChatGPT was used to import the google font. Link to conversation:
+// https://chat.openai.com/c/617eefc1-e74a-4138-994e-3c8213985ecf
+
+// import Google Fonts stylesheet
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "https://fonts.googleapis.com/css2?family=Playball&display=swap";
+document.head.appendChild(link);
+const style = document.createElement("style");
+style.textContent = `.slogan {font-family: 'Playball', cursive;}`;
+document.head.appendChild(style);
 
 // Menu data. An array of objects where each object represents a menu item. Each menu item has an id, title, description, image name, and price.
 // You can use the image name to get the image from the images folder.
 const menuItems = [
   {
     id: 1,
-    title: 'Gyoza',
-    description: 'Japanese dumplings',
-    imageName: 'gyoza.png',
+    title: "Gyoza",
+    description: "Japanese dumplings",
+    imageName: "gyoza.png",
     price: 5.99,
   },
   {
     id: 2,
-    title: 'Sushi',
-    description: 'Japanese rice rolls',
-    imageName: 'sushi.png',
+    title: "Sushi",
+    description: "Japanese rice rolls",
+    imageName: "sushi.png",
     price: 6.99,
   },
   {
     id: 3,
-    title: 'Ramen',
-    description: 'Japanese noodle soup',
-    imageName: 'ramen.png',
+    title: "Ramen",
+    description: "Japanese noodle soup",
+    imageName: "ramen.png",
     price: 7.99,
   },
   {
     id: 4,
-    title: 'Matcha Cake',
-    description: 'Japanese green tea cake',
-    imageName: 'matcha-cake.png',
+    title: "Matcha Cake",
+    description: "Japanese green tea cake",
+    imageName: "matcha-cake.png",
     price: 4.99,
   },
   {
     id: 5,
-    title: 'Mochi',
-    description: 'Japanese rice cake',
-    imageName: 'mochi.png',
+    title: "Mochi",
+    description: "Japanese rice cake",
+    imageName: "mochi.png",
     price: 3.99,
   },
   {
     id: 6,
-    title: 'Yakitori',
-    description: 'Japanese skewered chicken',
-    imageName: 'yakitori.png',
+    title: "Yakitori",
+    description: "Japanese skewered chicken",
+    imageName: "yakitori.png",
     price: 2.99,
   },
   {
     id: 7,
-    title: 'Takoyaki',
-    description: 'Japanese octopus balls',
-    imageName: 'takoyaki.png',
+    title: "Takoyaki",
+    description: "Japanese octopus balls",
+    imageName: "takoyaki.png",
     price: 5.99,
   },
   {
     id: 8,
-    title: 'Sashimi',
-    description: 'Japanese raw fish',
-    imageName: 'sashimi.png',
+    title: "Sashimi",
+    description: "Japanese raw fish",
+    imageName: "sashimi.png",
     price: 8.99,
   },
   {
     id: 9,
-    title: 'Okonomiyaki',
-    description: 'Japanese savory pancake',
-    imageName: 'okonomiyaki.png',
+    title: "Okonomiyaki",
+    description: "Japanese savory pancake",
+    imageName: "okonomiyaki.png",
     price: 6.99,
   },
   {
     id: 10,
-    title: 'Katsu Curry',
-    description: 'Japanese curry with fried pork',
-    imageName: 'katsu-curry.png',
+    title: "Katsu Curry",
+    description: "Japanese curry with fried pork",
+    imageName: "katsu-curry.png",
     price: 9.99,
-  }
+  },
 ];
-
 
 function App() {
   return (
-    <div>
-      <h1>Menu</h1>
-      <div className="menu">
-        {/* Display menu items dynamicaly here by iterating over the provided menuItems */}
-        <MenuItem title={menuItems[0].title} /> {/* Example for how to use a component */}
-      </div>
+    <div className="container">
+      <img src="restaurant-logo.png" alt="logo image" className="logo" />
+      <div className="slogan">Blooming With Authentic Japanese Flavor</div>
+      <h1 className="UT-message">Bringing Asian Cuisine to UT</h1>
+      {/* Display menu items dynamically here by iterating over the provided menuItems */}
+      {menuItems.map((item) => (
+        <div className="row">
+          <MenuItem
+            className="menu-item"
+            key={item.id}
+            title={item.title}
+            imgSrc={item.imageName}
+            description={item.description}
+            price={item.price}
+          />
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,16 +1,29 @@
-import React from 'react';
-
+import React from "react";
 
 // This is a functional component that represents a single menu item. It currently takes in the title and displays it in an h2 element.
 // Modify the component to take in all the other properties of a menu item you need and display them in the component.
 // Use bootstrap to style the elements so that it looks like the mockup in the assignment.
 // Hint: You can use the image name to get the image from the images folder.
-const MenuItem = ({ title }) => {
-    return (
-        <div>
-            <h2>{title}</h2>
+const MenuItem = ({ title, imgSrc, description, price }) => {
+  return (
+    <>
+      <div className="col-5">
+        <img src={"/images/" + imgSrc} className="img-responsive" alt={title} />
+      </div>
+      <div className="col-7">
+        <h2 className="item-name">{title}</h2>
+        <p className="description">{description}</p>
+        <div className="row price-and-bt">
+          <div className="col-6">
+            <p className="price">Price: ${price}</p>
+          </div>
+          <div className="col-6">
+            <button className="add-button">Add</button>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default MenuItem;
